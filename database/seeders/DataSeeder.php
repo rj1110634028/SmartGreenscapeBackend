1a960ede61043d3bbe507e0d32ea8f16826774fc
RJ Liu <jo901025@gmail.com>
2023-12-31 13:39:49 +0800

    feat: add data seeder.

--- database/seeders/DataSeeder.php
+++ database/seeders/DataSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Data;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,35 @@ class DataSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                "temperature" => 23,
+                "humidity" => 33,
+                "soil_humidity" => 33,
+                "mac_address" => "A0:B7:65:DE:0C:08",
+                "time" => "2023-12-26 11:34:00"
+            ], [
+                "temperature" => 25,
+                "humidity" => 35,
+                "soil_humidity" => 35,
+                "mac_address" => "A0:B7:65:DE:0C:08",
+                "time" => "2023-12-26 11:40:00"
+            ], [
+                "temperature" => 23,
+                "humidity" => 33,
+                "soil_humidity" => 33,
+                "mac_address" => "A0:B7:65:DE:0C:08",
+                "time" => "2023-12-26 12:34:00"
+            ], [
+                "temperature" => 21,
+                "humidity" => 31,
+                "soil_humidity" => 31,
+                "mac_address" => "A0:B7:65:DE:0C:08",
+                "time" => "2023-12-26 12:34:00"
+            ]
+        ];
+        foreach ($data as $value) {
+            Data::create($value);
+        }
     }
 }
